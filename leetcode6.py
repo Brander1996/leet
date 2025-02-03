592a9afa61da169531e7836622469012f9c078fb
Brander <x.v.leeuwen@gmail.com>
2025-02-03 22:16:47 +0100

    message

--- leetcode6.py
+++ leetcode6.py
@@ -2,5 +2,19 @@
 
 class Solution(object):
 
-    def convert(self, s: str, numRows: int) -> str:
+    def convert(self, string, numRows):
+        pointer = 0
+        currentCoordinates = (0,0)
+        stringLength = len(string)
+        while(pointer < stringLength):
+            self.nextZigZagCoordinatites(currentCoordinates, numRows)
+
+
+    def nextZigZagCoordinatites(self, coordinatines, numRows):
+        if(coordinatines[0] == numRows - 1 ):
+            return(coordinatines[0] - 1, coordinatines[1] + 1)
+
+
+
+something
         
