c73c5960a624deaf169fa62020b3fa95c47cbb9b
Brander <x.v.leeuwen@gmail.com>
2025-02-03 20:35:07 +0100

    zigzag exercise start

---
+++ BinTree.py
@@ -0,0 +1,17 @@
+import node
+
+class BinTree:
+
+
+
+    def addElement(self, node):
+        currentNode = self.getRoot()
+        while(currentNode != Leaf)
+        if 
+        if(node.leftChild.getKey() <= node.getKey()):
+            if
+
+
+
+    def nextElement():
+
---
+++ leetcode6.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class Solution(object):
+
+    def convert(self, s: str, numRows: int) -> str:
+        
