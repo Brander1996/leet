b7601f3e5eda40803d3d8a165a2d6c5282e8080e
Brander1996 <x.v.leeuwen@gmail.com>
2025-02-04 19:02:22 +0100

    palindrome accepted

--- leetcode9.py
+++ leetcode9.py
@@ -1,81 +1,34 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 
-class Solution(object):
-
-    def removeWhiteSpace(self, input_string):
-        pointer = 0 
-        while(pointer<len(input_string)):
-            if(input_string[pointer] != " "):
-                return input_string[pointer:]  
-            pointer = pointer + 1
-        return ""
-
-    
-    def isNegative(self, input_string):
-        if(len(input_string)==0):
-            return False
-        if(input_string[0] =="-"):
-            return True
-        else:
-            return False
-    
-    def removeSign(self, input_string):
-        if(len(input_string)==0):
-            return ""
-        if(input_string[0] =="-" or input_string[0] == "+"):
-            return input_string[1:]
-        else: 
-            return input_string
-    
-    def skipZeros(self, input_string):
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        length_input = len(x)
+        middle = length_input//2
+        odd = length_input%2==1
         pointer = 0
-        while(pointer<len(input_string)):
-            if(input_string[pointer] != "0"):
-                return input_string[pointer:]
-            pointer = pointer + 1
-        return ""
-    
-  
-    def provideLeadingDigits(self, input_string):
-        if(len(input_string)==0):
-            return ""
-        else:
-            pointer = 0
-            while(pointer<len(input_string)):
-                if(not input_string[pointer].isdigit()):
-                    return input_string[:pointer]
+        stack = []
+        for i in range(length_input):
+            if(pointer < middle):
+                stack.append(x[pointer])
                 pointer = pointer + 1
-            return input_string
-    
-    def roundNumber(self, number):
-        negative_boundry = -(2**31)
-        positive_boundry =2**31 - 1
-        if(number <= negative_boundry):
-            return negative_boundry
-        if (number >= positive_boundry):
-            return positive_boundry
-        return number
-    
-    def myAtoi(self, input_string):
-        without_whitespace = self.removeWhiteSpace(input_string)
-        is_negative =self.isNegative(without_whitespace)
-        without_sign = self.removeSign(without_whitespace)
-        without_zeros = self.skipZeros(without_sign)
-        digits= self.provideLeadingDigits(without_zeros)
-        if(digits == ""):
-            return 0
-        number = int(digits)
-        if(is_negative):
-            number = -number
-        return self.roundNumber(number)
+            elif(odd and pointer == middle):
+                pointer = pointer + 1
+            else:
+                if(stack.pop() != x[pointer]):
+                    return False
+                pointer = pointer + 1
+        return True
+                
+                
 
-          
 
+        
 def main():
     s = Solution()
-    print(s.myAtoi("10"))
+    result = s.isPalindrome(-1)
+    print(result)
 
 
 if(__name__=="__main__"):
     main()
-
